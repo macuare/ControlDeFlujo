731744565e76dbd10f95c13e57d1fac8aa1d988f
CusattiAndy <andyaacm@gmail.com>
2024-08-01 09:48:16 -0300

    Desafio realizado

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+    }
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
